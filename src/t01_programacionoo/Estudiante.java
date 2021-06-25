4c735a59fac8ff125ab732cad957e8fd3a84cce2
usuario <usuario@192.168.1.6>
2021-06-25 18:08:16 -0500

    Ejemplos pilares POO

--- src/t01_programacionoo/Estudiante.java
+++ src/t01_programacionoo/Estudiante.java
@@ -1,5 +1,26 @@
 package t01_programacionoo;
 
 public class Estudiante {
+	
+	/*Encapsulacion*/
+	
+	private String Nombre;
+	private String Apellido;
+	
+	/*Abstraccion*/
+	
+	void matricularse(String Documentos, String Dia) {
+		String cedula;
+		String genero;
+		String carrera;
+	}
+	
+	/*Polimorfismo*/
+	
+	void matricularse(String Facultad) {
+		String carrera;
+		String cedula;
+		
+	}
 
 }
