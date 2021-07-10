2030735f47e53b0a5360339f0f3de9a41796e2ad
usuario <usuario@192.168.1.3>
2021-07-09 19:40:28 -0500

    Ejercicio tabla del 7

---
+++ src/diagramas/de/flujo/Main3ej3.java
@@ -0,0 +1,20 @@
+package diagramas.de.flujo;
+
+public class Main3ej3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//imprimir la tabla del 7 hasta el 12
+		int i;
+		int j;
+		int M;
+		for(i=7; i<=7;i++) {
+			for(j=0;j<=12;j++) {
+				M=i*j;
+				System.out.println(M);
+			}
+			
+		}
+	}
+
+}
