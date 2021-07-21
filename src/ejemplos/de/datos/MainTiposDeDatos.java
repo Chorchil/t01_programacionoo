90fbb862648b13b00faee124debac81fb2aaaf8a
usuario <usuario@192.168.1.3>
2021-07-21 18:54:00 -0500

    Taller tipos de datos

--- src/ejemplos/de/datos/MainTiposDeDatos.java
+++ src/ejemplos/de/datos/MainTiposDeDatos.java
@@ -4,7 +4,15 @@ public class MainTiposDeDatos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//tipos de datos
+		short canEstudi= 1000; 
+		byte  entradaCir=0;
+		int  canCiudada=100000;
+		long canMundo=10000000;
+		float sueldo=(float) 1200.56;
+		double saldoBanco =10000000.67;
+		char opcionMenu='H';
+		boolean casado=true;
 	}
 
 }
